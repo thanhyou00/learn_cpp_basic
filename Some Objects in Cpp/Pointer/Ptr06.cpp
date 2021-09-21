bf8fbc53c053aced5816a58ddf56067dec8e1fec
thanhyou00 <truc06082000@gmail.com>
2021-09-21 22:50:12 +0700

    thanhyou00

--- Some Objects in Cpp/Pointer/Ptr06.cpp
+++ Some Objects in Cpp/Pointer/Ptr06.cpp
@@ -1,19 +1,32 @@
-#include<bits/stdc++.h>
+#include<iostream>
+
 using namespace std;
 
-// Mang 2 chieu
+// Con tro tro den con tro ( Con tro cap 2 )
 
-void showArr(int arr[5][5]) {
-	for(int i=0;i<5;i++) {
-		for(int j=0;j<5;j++) {
-			cout<<*(*(arr+i)+j) <<" ";
+int main() {
+	int** arr;
+	int n,m;
+	cin>>n>>m;
+	// Cap phat dong
+	arr = new int* [n];
+	for(int i=0;i<n;i++){
+		arr[i] = new int [m];
+	}
+	// Nhap di lieu cho tung phan tu
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	// In ra man hinh 
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			cout<<*(*(arr+i)+j)<<" ";
 		}
 		cout<<endl;
 	}
-}
-
-int main(){ 
-	int a[5][5] = {};
-	showArr(a);
-    return 0;
+	// Giai phong bo nho cho mang cac con tro
+	delete[] arr;
+	return 0;
 }
---
+++ Some Objects in Cpp/Pointer/Ptr07.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int add =5;
+	string name = to_string(add);
+	cout<<name;
+	return 0;
+}
+
