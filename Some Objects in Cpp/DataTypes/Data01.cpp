fbbfa5c4a350e6a7d9e43dbf7d333e05636a1f3a
thanhyou00 <truc06082000@gmail.com>
2021-09-29 11:12:37 +0700

    thanhyou00

--- Some Objects in Cpp/DataTypes/Data01.cpp
+++ Some Objects in Cpp/DataTypes/Data01.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 /*
 * Cac kieu du lieu co ban - Fundamental Data Types
-	Kieu di lieu				Y nghia					  Kich thuoc (In Byte)
-	int							Integer					  2 hoac 4
-	float						Floating-point			  4
-	double						Double Floating point	  8
-	char						Character				  1
-	wchar_t						Wide Character			  2
-	bool						Boolean					  1
-	void						Empty					  0
+	Kieu di lieu - Y nghia - Kich thuoc (In Byte)
+	int - Integer - 2 hoac 4
+	float - Floating-point - 4
+	double - Double Floating point - 8
+	char - Character - 1
+	wchar_t	- Wide Character - 2
+	bool - Boolean - 1
+	void - Empty - 0
 */
 int main() {
 
