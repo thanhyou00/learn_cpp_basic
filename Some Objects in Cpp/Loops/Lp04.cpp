4fd7cb2da0163bae41511aec623306da8c8bf60a
thanhyou00 <truc06082000@gmail.com>
2021-09-29 09:45:54 +0700

    thanhyou00

--- Some Objects in Cpp/Loops/Lp04.cpp
+++ Some Objects in Cpp/Loops/Lp04.cpp
@@ -4,7 +4,25 @@ using namespace std;
 // do...while loop
 
 int main() {
+// Vi du : In ra cac so tu 1 den 5
 
+	int i = 1;
+	do {
+		cout<<i<<" ";
+		i++;
+	}while(i<=5);
+/*
+Notes :
+- Dieu kien dung/sai thi cau lenh trong 'do' cung duoc thuc thi it nhat 1 lan
+- Thuc hien cau lenh trong 'do' truoc roi moi kiem tra dieu kien trong 'while', neu sai thi ngung vong lap , dung thi vong lap duoc chay tiep
+- Vi du : 
+	int i = 1;
+	do {
+		cout<<i<<" ";
+		i++;
+	}while(i>=5);
+=> Dieu kien sai i>=5 nhung khi chay se cho in ra ket qua la 1	
+*/	
 	return 0;
 }
 
