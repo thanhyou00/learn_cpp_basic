c37901757afdcb9ed407ef08677f9a14c222c4f5
thanhyou00 <truc06082000@gmail.com>
2021-09-29 13:35:36 +0700

    thanhyou00

---
+++ Some Objects in Cpp/DataTypes/Data02.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+* Cac kieu du lieu mo rong - Modified Data Types :
+- signed
+=> signed int, signed char ...
+- unsigned 
+=> unsigned int, unsigned char ...
+- short
+=> short int, short char ...
+- long
+=> long long , long int ...
+*/
+int main() {
+	long long num = 12345678999;
+	cout<<sizeof(long long)<<endl;
+	cout<<num<<endl;
+	return 0;
+}
+
