7e0685f301bcc23a5791505024920ef696b2b4c4
thanhyou00 <truc06082000@gmail.com>
2021-10-04 07:45:22 +0700

    thanhyou00

--- Some Objects in Cpp/Structure/Str04.cpp
+++ Some Objects in Cpp/Structure/Str04.cpp
@@ -14,12 +14,14 @@ int main() {
 		
 	cin>>(*ptr).feet>>(*ptr).inch; // Nhap vao gia tri cho cac bien trong struct
 	cout<<(*ptr).feet<<" and "<<(*ptr).inch<<endl; // In ra man hinh
+	cout<<temp.feet<<" and "<<temp.inch<<endl;
+	cout<<ptr->feet<<" and "<<ptr->inch<<endl;
 /*
 * Giai thich :
 - *ptr : Bien con tro co kieu du lieu struct
 - temp : Bien thuong co kieu du lieu struct
 - ptr = &temp : Gia tri cua bien con tro duoc gan bang dia chi cua bien thuong
-=> Ban chat cua no la : (*ptr).feet <=> temp.feet va (*ptr).inch <=> temp.inch
+=> 3 cach tuong duong nhau dung de truy cap : (*ptr).feet <=> ptr->feet <=> temp.feet va (*ptr).inch <=> ptr->inch <=> temp.inch
 =>> Neu ban chua hieu hay quay lai phan Pointer de on tap lai nhe :33
 */	
 	return 0;
